0ed23772373c15e24ae4e22da129d0fd5282cd0e
Artis-Dev <admin@moonart.lt>
2022-02-16 16:28:16 +0200

    Add functions to fetch data from API

--- main.js
+++ main.js
@@ -7,26 +7,90 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/api.js":
+/*!********************!*\
+  !*** ./src/api.js ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n// Current:\n// City and country - weatherData.name, weatherData.sys.country\n// Temp - weatherData.main.temp\n// Temp feels like - weatherData.main.feels_like\n// Temp description - weatherData.weather.main, weatherData.weather.description\n// Wind speed - weatherData.wind.speed\n// Wind speed degree - weatherData.wind.deg\n// Chance of rain -\n// Humidity - weatherData.main.humidity\n// Date and time - new Date()\n// Sunrise time - weatherData.sys.sunrise\n// Sunset time- weatherData.sys.sunset\n//\n// Daily (7 days):\n// Day temp\n// Night temp\n// Temp description\n// Wind?\n//\n// Hourly (24 hours):\n// Temp\n// Temp description\n// Chance of rain\n// Wind?\n\nconst api = (() => {\n  async function getCityData(query) {\n    const response = await fetch(\n      `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=c93fd1817f3fbe42aeac0a63076603b9`,\n      { mode: 'cors' },\n    );\n    const data = await response.json();\n    const { coord } = data;\n    const city = data.name;\n    const { country } = data.sys;\n    return { coord, city, country };\n  }\n\n  async function getForecastData(coord, units = 'metric') {\n    const response = await fetch(\n      `https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&exclude=alerts,minutely&units=${units}&appid=c93fd1817f3fbe42aeac0a63076603b9`,\n      { mode: 'cors' },\n    );\n    const data = await response.json();\n    return data;\n  }\n\n  async function getData(query) {\n    const cityData = await getCityData(query);\n    const forecastData = await getForecastData(cityData.coord);\n    return { cityData, forecastData };\n  }\n\n  return {\n    getData,\n  };\n})();\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (api);\n\n\n//# sourceURL=webpack://weather-app/./src/api.js?");
+
+/***/ }),
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("console.log('Hello World!');\n\nasync function getWeather(location) {\n  const fetchUrl = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c93fd1817f3fbe42aeac0a63076603b9`;\n\n  try {\n    const response = await fetch(fetchUrl, { mode: 'cors' });\n    const weatherData = await response.json();\n    console.log(weatherData);\n  } catch (err) {\n    alert(err);\n  }\n}\n\ngetWeather('Lhasa');\n\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _api__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./api */ \"./src/api.js\");\n\n\n_api__WEBPACK_IMPORTED_MODULE_0__[\"default\"].getData('Vilnius').then((response) => {\n  console.log(response);\n});\n\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
