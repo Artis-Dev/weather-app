1fcae11fad7334455805e3b5685418f4245a5a0b
ArtisDev <artisdev@proton.me>
2023-12-07 16:43:24 +0200

    Bump dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,21 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  rules: {
-  },
+  rules: {},
 };
